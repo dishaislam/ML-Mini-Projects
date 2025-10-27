900e058005018215b0c0eb616fa731d9df00fcaa
dishaislam <dsharminislam@gmail.com>
2025-10-27 15:42:21 +0600

    added KNN

--- ch1.py
+++ ch1.py
@@ -2,14 +2,16 @@ import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 from sklearn.linear_model import LinearRegression
+from sklearn.neighbors import KNeighborsRegressor
 
 # Download and prepare the data
 data_root = "https://github.com/ageron/data/raw/main/"
 lifesat = pd.read_csv(data_root + "lifesat/lifesat.csv")
+
+lifesat2= lifesat.copy
 X = lifesat[["GDP per capita (USD)"]].values
 y = lifesat[["Life satisfaction"]].values
 
-# Visualize the data
 lifesat.plot(kind='scatter', grid=True,
 x="GDP per capita (USD)", y="Life satisfaction")
 plt.axis([23_500, 62_500, 4, 9])
@@ -18,9 +20,13 @@ plt.show()
 # Select a linear model
 model = LinearRegression()
 
+model2 = KNeighborsRegressor()
+
 # Train the model
 model.fit(X, y)
+model2.fit(X, y)
 
 # Make a prediction for Cyprus
 X_new = [[37_655.2]] # Cyprus' GDP per capita in 2020
 print(model.predict(X_new)) # output: [[6.30165767]]
+print(model2.predict(X_new))
